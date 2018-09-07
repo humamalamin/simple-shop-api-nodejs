62f7983b3c713d70cbf4e0c9c3c1c37f52ffd145
humam al amin <humamalamin13@gmail.com>
2018-09-07 11:14:58 +0700

    Parsing Body & handling CORS

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -1,6 +1,7 @@
 const express   = require('express');
 const router    = express.Router();
 
+// Handle incoming Get Request to /orders
 router.get('/',(req, res, next) => {
     res.status(200).json({
         message: 'Orders where fetched'
@@ -8,8 +9,12 @@ router.get('/',(req, res, next) => {
 });
 
 router.post('/',(req, res, next) => {
+    const order = {
+        productId : req.body.productId,
+        quantity : req.body.quantity
+    }
     res.status(201).json({
-        message: 'Orders was created'
+        order : order
     });
 });
 
--- app.js
+++ app.js
@@ -1,10 +1,45 @@
 const express   = require('express');
 const app       = express();
+const morgan    = require('morgan');
+const bodyParser= require('body-parser');
 
 const productRoutes = require('./api/routes/products');
 const ordersRoutes  = require('./api/routes/orders');
 
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({extended : false}));
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+
+    if(req.method === 'OPTIONS')
+    {
+        res.header('Access-Control-Allow-Methods','PUT, POST, DELETE, PATCH, GET');
+        returnres.status(200).json({
+        });
+    }
+    next();
+});
+
+// Routes which should handle requests
 app.use('/products',productRoutes);
 app.use('/orders',ordersRoutes);
+
+app.use((req, res, next) => {
+    const error = new Error('Not Found');
+    error.status = 404;
+    next(error);
+});
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error : {
+            message : error.message
+        }
+    });
+});
 
 module.exports = app;
