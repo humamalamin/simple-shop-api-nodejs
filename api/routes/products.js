7a13fbbdc29def4764801ed98f520244cdf1f647
humam al amin <humamalamin13@gmail.com>
2019-03-04 10:52:17 +0700

    Upload image and filter type file

--- api/routes/products.js
+++ api/routes/products.js
@@ -1,12 +1,38 @@
 const express   = require('express');
 const router    = express.Router();
 const mongoose  = require('mongoose');
+const multer    = require('multer');
+
+const storage   = multer.diskStorage({
+    destination: function(req,file, cb){
+        cb(null, './uploads/');
+    },
+    filename: function(req,file, cb){
+        cb(null, new Date().toISOString() + file.originalname)
+    }
+})
+
+const fileFilter    = (req, file, cb) => {
+    //reject file
+    if(file.mimetype == 'image/jpeg'|| file.mimetype == 'image/png'){
+        cb(null, true);   
+    }else{
+        cb(new Error("Type Image foul."), false);
+    }
+};
+
+const upload    = multer({storage: storage, 
+    limit: {
+        fileSize:1024 * 1024 * 2
+    },
+    fileFilter: fileFilter
+})
 
 const Product   = require('../models/product');
 
 router.get('/',(req, res, next) => {
     Product.find()
-    .select('name price_id')
+    .select('name price _id productImage')
     .exec()
     .then(docs => {
         const response = {
@@ -15,6 +41,7 @@ router.get('/',(req, res, next) => {
                 return {
                     name: doc.name,
                     price: doc.price,
+                    productImage: doc.productImage,
                     _id: doc._id,
                     request: {
                         type: "GET",
@@ -23,11 +50,8 @@ router.get('/',(req, res, next) => {
                 }
             })
         };
-        // if(docs.length > 0){
-            res.status(200).json(response);
-        // }else{
-        //     res.status(404).json({message:"no entries found"});
-        // }
+        res.status(200).json(response);
+        
     })
     .catch(err => {
         console.log(err);
@@ -35,20 +59,23 @@ router.get('/',(req, res, next) => {
     });
 });
 
-router.post('/',(req, res, next) => {
-
+router.post('/',upload.single('productImage'),(req, res, next) => {
+    console.log(req.file)
     const product   = new Product({
         _id: new mongoose.Types.ObjectId(),
         name: req.body.name,
-        price: req.body.price
+        price: req.body.price,
+        productImage: req.file.path
     });
-    product.save().then(result => {
+    product.save()
+    .then(result => {
         console.log(result);
         res.status(201).json({
             message:"Created product success",
             createdProduct: {
                 name: result.name,
                 price: result.price,
+                productImage: result.productImage,
                 _id: result._id,
                 request: {
                     type: 'GET',
@@ -68,7 +95,7 @@ router.post('/',(req, res, next) => {
 router.get('/:productId',(req, res, next) => {
     const id    = req.params.productId;
     Product.findById(id)
-    .select('name price _id')
+    .select('name price _id productImage')
     .exec()
     .then(doc => {
         console.log(doc);
